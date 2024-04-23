6c3cc6f1e75e6351577af46214f7b2b9e3de5c8f
Ekaterina Polushina <terryrinkelly@gmail.com>
2024-04-23 22:12:08 +0300

    1.Поправлен импорт -> from selene import browser, command, have, by. 2.Поправлены орфографические ошибки "Registation"Page -> RegistrationPage, adress-> address. 3.Скорректирован импорт изображения. 4. Добавлен RegistrationPage в тест 5. Убран return self

--- model/pages/registration_page.py
+++ model/pages/registration_page.py
@@ -1,12 +1,10 @@
-from fileinput import filename
-
 from selene import command, by, have
 from selene.support.shared import browser
 
 import resource
 
 
-class RegistationPage:
+class RegistrationPage:
     def __init__(self):
         self.first_name = browser.element('#firstName')
         self.last_name = browser.element('#lastName')
@@ -22,74 +20,59 @@ class RegistationPage:
 
     def open(self):
         browser.open('/automation-practice-form')
-        return self
 
     def fill_first_name(self, value):
         self.first_name.type(value)
-        return self
 
     def fill_last_name(self, value):
         self.last_name.type(value)
-        return self
 
     def fill_email(self, value):
         self.email.type(value)
-        return self
 
     def fill_click_element(self, value):
         browser.element(by.text(value)).perform(command.js.click)
-        return self
 
     def fill_user_gender(self, value):
         self.fill_click_element(value)
-        return self
 
     def fill_user_number(self, value):
         self.user_number.type(value)
-        return self
 
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type(month)
         browser.element('.react-datepicker__year-select').type(year)
         browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click()
-        return self
 
     def fill_subjects_input(self, value):
         browser.element('#subjectsInput').set_value(value).press_enter()
-        return self
 
     def fill_hobbies(self, value):
         self.fill_click_element(value)
-        return self
 
-    def fill_upload_picture(self, filename):
-        browser.element('#uploadPicture').send_keys(resource.path(filename))
-        return self
+    def fill_upload_picture(self, file):
+        self.upload_picture.set_value(resource.path(file))
 
     def fill_current_address(self, value):
         self.current_address.set_value(value)
-        return self
 
     def fill_state(self, value):
         browser.element('#state').perform(command.js.scroll_into_view)
         self.state.click().element(by.text(value)).click()
-        return self
 
     def fill_city(self, value):
         self.city.click().element(by.text(value)).click()
-        return self
 
     def fill_submit(self):
         self.submit.click()
-        return self
 
     def should_finish_form_title(self, value):
         browser.element('.modal-title').should(have.text(value))
 
     def should_registered_user_with(self, full_name, email,
                                     user_gender, user_number, date_of_birth,
-                                    subjects_input, hobbies,  upload_picture, current_address, state_and_city):
+                                    subjects_input, hobbies, upload_picture, current_address, state_and_city):
         browser.element('.table').all('td').even.should(
             have.exact_texts(
                 full_name,
@@ -107,5 +90,3 @@ class RegistationPage:
 
     def fill_close_large_modal(self):
         browser.element('#closeLargeModal').perform(command.js.click)
-
-
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,42 +1,36 @@
-from selene import browser, by, command, have
-import os.path
+from model.pages.registration_page import RegistrationPage
 
 
 def test_form():
-    browser.open('/automation-practice-form')
-    # Заполнение формы
-    browser.element('#firstName').type('Gandalf')
-    browser.element('#lastName').type('Grey')
-    browser.element('#userEmail').type('gandalf@gmail.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('1234567890')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element(by.text('March')).click()
-    browser.element('.react-datepicker__year-select').click().element(by.text('2024')).click()
-    browser.element('.react-datepicker__day--030').click()
-    browser.element('#subjectsInput').type('Arts').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/Gendolf.jpg'))
-    browser.element('#currentAddress').type('adress')
-    browser.element('#currentAddress').perform(command.js.scroll_into_view).click()
-    browser.element('#state').click().element(by.text('Haryana')).click()
-    browser.element('#city').click().element(by.text('Karnal')).click()
-    browser.element('#submit').click()
+    registration_page = RegistrationPage()
+    registration_page.open()
 
-    # Проверка var1 - этот постоянно падает
-    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
-    browser.element('.table').all('td').even.should(
-        have.exact_texts(
-            'Gandalf Grey',
-            'gandalf@gmail.com',
-            'Male',
-            '1234567890',
-            '30 March,2024',
-            'Arts',
-            'Reading',
-            'Gendolf.jpg',
-            'adress',
-            'Haryana Karnal'
-        )
+    registration_page.fill_first_name('Gandalf')
+    registration_page.fill_last_name('Grey')
+    registration_page.fill_email('gandalf@gmail.com')
+    registration_page.fill_user_gender('Male')
+    registration_page.fill_user_number('1234567890')
+    registration_page.fill_date_of_birth('2024', 'March', '30')
+    registration_page.fill_subjects_input('Arts')
+    registration_page.fill_hobbies('Reading')
+    registration_page.fill_upload_picture('Gendolf.jpg')
+    registration_page.fill_current_address('adress')
+    registration_page.fill_state('Haryana')
+    registration_page.fill_city('Karnal')
+    registration_page.fill_submit()
+
+    registration_page.should_finish_form_title('Thanks for submitting the form')
+    registration_page.should_registered_user_with(
+        'Gandalf Grey',
+        'gandalf@gmail.com',
+        'Male',
+        '1234567890',
+        '30 March,2024',
+        'Arts',
+        'Reading',
+        'Gendolf.jpg',
+        'adress',
+        'Haryana Karnal'
     )
-    browser.element('#closeLargeModal').perform(command.js.click)
+
+    registration_page.fill_close_large_modal()
